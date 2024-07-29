38a0bcd6d3967791a59239b7a35b5a0f7658bf49
KiwiSorbet <galaxygum51@gmail.com>
2024-07-28 20:00:01 -0400

    changed location to the home directory

--- config.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# add symlinks for config files that need to be in the home directory
-rm -rf ~/.bashrc && ln -s ~/.config/bash/.bashrc ~/.bashrc
-rm -rf ~/.bash_aliases && ln -s ~/.config/bash/.bash_aliases ~/.bash_aliases
