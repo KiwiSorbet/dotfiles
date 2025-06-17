9984d9b7f1a2d21c8d8a3dde5557fe733b557f0e
KiwiSorbet <galaxygum51@gmail.com>
2025-06-17 04:57:02 -0400

    changed config script so it can work when called from any directory

--- config.sh
+++ config.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPO_DIR="$(dirname "$SCRIPT_DIR")"
+
 # Move all files from repo to ~/.config
-cp -r * ~/.config/
+cp -r "$REPO_DIR"/* ~/.config/
 
 # Symlinks
 ln -sf ~/.config/bash/.bashrc ${HOME}/.bashrc
